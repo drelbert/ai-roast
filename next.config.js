45e70535d1b763a45e513e12017e3f3eb96c247b
drelbert <danrelbert@gmail.com>
2023-08-03 14:24:32 -0400

    updated next.config

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // uncomment for deploy
-  // typescript: {
-  //     ignoreBuildErrors: true,
-  // },
-  // eslint: {
-  //     ignoreDuringBuilds: true,
-  //     ignoreBuildErrors: true
-  // }
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+    ignoreBuildErrors: true,
+  },
 }
 
 module.exports = nextConfig
